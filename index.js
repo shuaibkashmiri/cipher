49c2b1d9efaef69add67eb566b27d86e885933ee
Shoaib Mushtaq Bhat <114304104+shuaibkashmiri@users.noreply.github.com>
2026-02-04 14:56:47 +0530

    operators

--- index.js
+++ index.js
@@ -40,7 +40,50 @@
 // console.log(fistname + " " + lastname);
 // console.log(`${firstname} ${lastname}`);
 
-const name = "Shoaib";
-const age = 26;
+// const name = "Shoaib";
+// const age = 26;
 
-console.log(`My name is ${name} and my age is ${age}`);
+// console.log(`My name is ${name} and my age is ${age}`);
+
+// airthmatic operators
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+// comparison operators
+
+// let x = 5;
+// let y = 5;
+
+// console.log(x != y);
+// console.log(x == y);
+// console.log(x === y);
+// console.log(x <= y);
+// console.log(x >= y);
+
+// logical operators
+
+// console.log(false || false);
+// console.log(false && false);
+// console.log(!false);
+
+// let numOne = Number(prompt("Enter First Number"));
+// let numTwo = Number(prompt("Enter Second Number"));
+// console.log(`Sum is  ${numOne + numTwo}`);
+
+// reassing the values
+
+let a = 4;
+let b = 6;
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+a = a + b; //now a = 10
+b = a - b; // b=4
+a = a - b; // a=6
+console.log(`${a} ${b}`);
