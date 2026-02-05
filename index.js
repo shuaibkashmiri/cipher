89685909c3a35cc5c3113f8107c3deaabe2e2766
Shoaib Mushtaq Bhat <114304104+shuaibkashmiri@users.noreply.github.com>
2026-02-05 15:24:09 +0530

    conditional flow

--- index.js
+++ index.js
@@ -76,14 +76,87 @@
 
 // reassing the values
 
-let a = 4;
-let b = 6;
+// let a = 4;
+// let b = 6;
 
-// let temp = a;
-// a = b;
-// b = temp;
+// // let temp = a;
+// // a = b;
+// // b = temp;
 
-a = a + b; //now a = 10
-b = a - b; // b=4
-a = a - b; // a=6
-console.log(`${a} ${b}`);
+// a = a + b; //now a = 10
+// b = a - b; // b=4
+// a = a - b; // a=6
+// console.log(`${a} ${b}`);
+
+// conditional flow and decision making in js
+
+// const age = 17;
+
+// if (age >= 18) {
+//   console.log("You can Drive");
+// } else {
+//   console.log("You Cannot Drive");
+// }
+
+// const num = 6;
+
+// if (num % 2 == 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// const marks = 60;
+
+// if (marks >= 90) {
+//   console.log("A Grade");
+// } else if (marks >= 75) {
+//   console.log("B Grade");
+// } else {
+//   console.log("nograde");
+// }
+
+// const num1 = Number(prompt("Enter First Number"));
+// const num2 = Number(prompt("Enter Second Number"));
+// const opr = prompt("+, -,*,/");
+
+// if (opr == "+") {
+//   console.log(num1 + num2);
+// } else if (opr == "-") {
+//   console.log(num1 - num2);
+// } else if (opr == "*") {
+//   console.log(num1 * num2);
+// } else {
+//   console.log(num1 / num2);
+// }
+
+// if (userName == "admin") {
+//   if (password == "1234") {
+//     console.log("User Logged In ");
+//   } else {
+//     console.log("Password Incorrect");
+//   }
+// } else {
+//   console.log("No Admin");
+// }
+
+// if (userName == "admin" && password == "1234") {
+//   console.log("User Logged In Successfully");
+// } else {
+//   console.log("Invalid Credentials");
+// }
+
+// const userName = "u";
+// const role = "user";
+
+// if (role == "admin" || role == "superAdmin") {
+//   console.log(`Welcome ${userName} to the Admin Panel`);
+// } else {
+//   console.log(`Welcome ${userName} to Your Dashboard`);
+// }
+
+// ternary operator
+
+// const age = 15;
+
+// age >= 18 ? console.log("You Can Drive") : console.log("you Cannot Drive");
