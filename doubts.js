08d6f3527ff99b9377af9e09ed81ea0f300cefbb
Shoaib Mushtaq Bhat <114304104+shuaibkashmiri@users.noreply.github.com>
2026-02-21 15:14:55 +0530

    doubts

---
+++ doubts.js
@@ -0,0 +1,137 @@
+// var name = "Shoaib"
+// let name = "shoaib"
+// const num=8
+
+// datatypes
+
+// strings
+
+// let str = "JavaScript";
+// let str2 = "Doubts";
+
+// string methods
+// console.log(str + str2);
+// console.log(str);
+
+// number
+
+// const num=2
+// const num2=2.2
+
+// null and undefined
+
+// const a = null;
+// let b;
+
+// console.log(2 + 3);
+// console.log(3 - 1);
+// console.log(3 * 1);
+// console.log(6 / 2);
+// console.log(5 % 2);
+
+// console.log(3 > 6);
+// console.log(2 == "2");
+
+// let age = 16;
+
+// if (age > 18) {
+//   console.log("You can drive");
+// } else if (age == 18) {
+//   console.log("You have to wait for a year");
+// } else {
+//   console.log("You cant drive");
+// }
+
+//
+
+// loops
+
+// let a = 1;
+
+// while loop
+// while (a <= 10) {
+//   console.log(a);
+//   a++;
+// }
+
+// let a = 1;
+
+// do {
+//   console.log(a);
+//   a++;
+// } while (a >= 10);
+
+// debugger;
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// arrays
+
+// const arr = [2, 2.5, "Hello", true];
+
+// console.log();
+
+// arr.push("Hello");
+
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// const arr = [2, 2.5, "Hello", true];
+// console.log(arr[0]);
+
+// debugger;
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for of
+// for (const i of arr) {
+//   console.log(i);
+// }
+
+// let str = "Javascript";
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+// funtions
+
+// defining /declaring a function
+// function greetings() {
+//   console.log("Hello");
+// }
+
+// // call or invoke
+
+// greetings();
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const result = sum(4, 6);
+// console.log(result);
+
+// const greet = function () {
+//   console.log("Hello World");
+// };
+
+// greet();
+
+// const greetings = () => {
+//   console.log("Hello");
+// };
+
+// greetings();
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const sum = (a, b) => a + b;
+
+// console.log(sum(2, 3));
